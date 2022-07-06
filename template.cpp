ca9372b2487a059192a99f4e0d56ef5525e70dcd
batyrlank <75443631+batyrlank@users.noreply.github.com>
2022-07-06 18:52:15 +0600

    f

---
+++ template.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <fstream>
+#include <cassert>
+#include <cstring>
+#include <unordered_set>
+#include <unordered_map>
+#include <numeric>
+#include <ctime>
+#include <bitset>
+#include <complex>
+#include <chrono>
+#include <random>
+#include <functional>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define ff first
+#define ss second
+
+const int N = 1e6 + 7;
+const long long MOD = 1e9 + 7;
+
+void solve(){
+}
+
+int main(){
+}
